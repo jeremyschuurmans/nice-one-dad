21baeef2eff1b06175e7f6c4cec46befd91d58d8
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-11-30 13:51:07 -0800

    make xhr request to dad joke api

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let request = new XMLHttpRequest();
+function getJoke() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showJoke);
+  req.open('GET', 'https://icanhazdadjoke.com/');
+  req.setRequestHeader('Accept', 'application/json')
+  req.send();
+}
+
+function showJoke() {
+  console.log(this.responseText);
+}
