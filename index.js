fbb473aae8ac6a15ae0c5f38dd2ebaae5e5fe216
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-01 12:39:54 -0800

    finish refactor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getJoke() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showJoke);
   req.open('GET', 'https://icanhazdadjoke.com/');
-  req.setRequestHeader('Accept', 'application/json')
+  req.setRequestHeader('Accept', 'application/json');
   req.send();
 }
 
